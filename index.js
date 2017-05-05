5433e02ef70647c9c5571aa6a023e68aadac61de
hyh <ec.huyinghuan@gmail.com>
2017-05-05 09:41:37 +0800

    :bug: fix bug when mult compiler

--- index.js
+++ index.js
@@ -2,61 +2,57 @@
 const _url = require('url');
 const _path = require('path');
 const _fs = require('fs');
-const _babel = require('babel-core');
+const _coffee = require('coffee-script');
 
 //根据实际路径获取文件内容
-const getCompileContent = (cli, realFilePath, data, options, cb)=>{
+const getCompileContent = (cli, realFilePath, data, cb)=>{
   if(!_fs.existsSync(realFilePath)){
     return cb(null, null)
   }
   let fileContent = _fs.readFileSync(realFilePath, {encoding: 'utf8'})
   try{
-    var result = _babel.transform(fileContent, options);
+    let source = _coffee.compile(fileContent)
     data.status = 200
-    cb(null, result.code)
+    cb(null, source)
   }catch(e){
     cb(e)
   }
 }
 
 exports.registerPlugin = function(cli, options){
-  let defOptions = { "presets": ["es2015"] }
-  //继承
-  if(options){
-    Object.keys(options).forEach(function(key){
-      defOptions[key] = options[key]
-    })
-  }
+
 
   cli.registerHook('route:didRequest', (req, data, content, cb)=>{
+
     //如果不需要编译
     if(!/\.js$/.test(req.path)){
       return cb(null, content)
     }
     let fakeFilePath = _path.join(cli.cwd(), req.path);
     //替换路径为less
-    let realFilePath = fakeFilePath.replace(/(js)$/,'es6')
-    getCompileContent(cli, realFilePath, data, defOptions, (error, compileContent)=>{
+    let realFilePath = fakeFilePath.replace(/(js)$/,'coffee')
+
+    getCompileContent(cli, realFilePath, data, (error, compileContent)=>{
       if(error){return cb(error)};
-      //交给下一个处理器
       if(compileContent){
         content = compileContent
       }
+      //交给下一个处理器
       cb(null, content)
     })
   })
 
   cli.registerHook('build:doCompile', (buildConfig, data, content, cb)=>{
     let inputFilePath = data.inputFilePath;
-    if(!/(\.es6)$/.test(inputFilePath)){
+    if(!/(\.coffee)$/.test(inputFilePath)){
       return cb(null, content)
     }
 
-    getCompileContent(cli, inputFilePath, data, defOptions, (error, content)=>{
+    getCompileContent(cli, inputFilePath, data, (error, content)=>{
       if(error){return cb(error)};
       if(data.status == 200){
-        data.outputFilePath = data.outputFilePath.replace(/(\.es6)$/, ".js");
-        data.outputFileRelativePath = data.outputFileRelativePath.replace(/(\.es6)$/, ".js")
+        data.outputFilePath = data.outputFilePath.replace(/(\.coffee)$/, ".js");
+        data.outputFileRelativePath = data.outputFileRelativePath.replace(/(\.coffee)$/, ".js")
       }
       cb(null, content);
     })
